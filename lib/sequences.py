56f64535a1ac38ab0af40d2da4e0bfd346e0d683
Valerie <valeriejeptoo4@gmail.com>
2023-08-18 10:11:09 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  first_numbers = [0,1]  
+  if length == 0:
+    print([])
+  elif length == 1:
+    print(0)
+  elif length == 2:
+    return first_numbers
+  else:
+    for i in range (2, length):
+      first_numbers.append(first_numbers[i-1] + first_numbers[i-2])
+      return first_numbers
+   
